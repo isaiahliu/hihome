e22b3900264e9f091730e78dfcdd5837e1902c1a
Isaiah Liu <zqliu@hengtiansoft.com>
2017-02-14 15:51:23 +0800

    1

--- rest/security/src/main/java/org/trinity/hihome/Application.java
+++ rest/security/src/main/java/org/trinity/hihome/Application.java
@@ -2,10 +2,10 @@ package org.trinity.hihome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
-@EnableResourceServer
+@EnableAuthorizationServer
 public class Application {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ rest/security/src/main/java/org/trinity/hihome/security/AuthorizationServerConfiguration.java
@@ -0,0 +1,35 @@
+package org.trinity.hihome.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager m;
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(m);
+    }
+
+    @Override
+    public void configure(final AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+        security.checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+        super.configure(clients);
+
+        clients.inMemory().withClient("test").secret("testsec").scopes("read").authorizedGrantTypes("password");
+    }
+}
